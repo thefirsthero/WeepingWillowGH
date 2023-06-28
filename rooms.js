ef876fd5c8295abc26dfa47a7cc22a0639ebe5b0
thefirsthero <kingvukosi@outlook.com>
2023-06-28 14:50:05 +0200

    fixing book now buttons

--- rooms.js
+++ rooms.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var bookButtons = document.getElementsByClassName('toBill');
-    console.log(bookButtons)
-
+document.addEventListener('DOMContentLoaded', function() {
+    var bookButtons = document.querySelectorAll('.toBill');
+  
     for (let index = 0; index < bookButtons.length; index++) {
-        let button = bookButtons[index];
-        button.addEventListener('click', function () {
-            let container = button.parentNode.parentNode;
-            let money = container.querySelector('.amt').textContent.split('/')[0].trim();
-            let room = container.querySelector('.roomType').textContent.trim();
-            let details = {
-                money: money,
-                room: room
-            };
-            localStorage.setItem('amount', JSON.stringify(details));
-            location.href = 'billing.html';
-        });
+      let button = bookButtons[index];
+      button.addEventListener('click', function() {
+        let container = button.parentNode.parentNode;
+        let money = container.querySelector('.amt').textContent.split('/')[0].trim();
+        let room = container.querySelector('.roomType').textContent.trim();
+        let details = {
+          money: money,
+          room: room
+        };
+        localStorage.setItem('amount', JSON.stringify(details));
+        location.href = 'billing.html';
+      });
     }
-});
+  });
+  
